7c2ce97f92219f6f869e56c5dd08cc64f37826b2
Riley Flynn <riley@rileyflynn.me>
2018-10-07 15:46:36 -0230

    Made all events happen in the "/presence" namespace, so reconnecting works

--- WebRichPresence_Client/Client.py
+++ WebRichPresence_Client/Client.py
@@ -1,10 +1,10 @@
 import json
 import logging
 import os
-from typing import Dict, Optional, Callable
+from typing import Dict, Optional, Callable, Type
 
 from pypresence import Presence
-from socketIO_client import SocketIO
+from socketIO_client import SocketIO, BaseNamespace
 
 
 class Client(object):
@@ -18,9 +18,10 @@ class Client(object):
     _config: Dict[str, Optional[str]]
     _socket: SocketIO
     _logger: logging.Logger
-    _callbacks: Dict[str, Callable[..., None]]
+    _presence_callbacks: Dict[str, Callable[..., None]]
     _current_app_id: Optional[str]
     _rpc: Presence
+    _presence_namespace: Type[BaseNamespace]
 
     def __init__(self, port: int, hostname: str, config_path: str):
         """
@@ -38,12 +39,18 @@ class Client(object):
         logging.getLogger("socketIO-client").setLevel(logging.ERROR)
         self._logger.setLevel(logging.INFO)
 
-        # Define all the callback functions, so we don't have to write self._socket.on(...) a million times
-        self._callbacks = {
+        # Dynamically create the namespace object
+        self._presence_callbacks = {
             "authenticated": self._on_authenticated,
             "new_token": self._on_new_token,
-            "presence": self._on_presence
+            "presence": self._on_presence,
+            "connect": self._on_connected,
+            "reconnect": self._on_reconnect
         }
+
+        self._presence_namespace = type("PresenceNamespace", (BaseNamespace, ), {
+            "on_" + k: v for k, v in self._presence_callbacks.items()
+        })
 
         if not os.path.isfile(config_path):
             self._config = {
@@ -64,7 +71,7 @@ class Client(object):
         self._rpc = Presence(app_id)
         self._rpc.connect()
         self._current_app_id = app_id
-    
+
     def _on_presence(self, app_id: str, presence: dict):
         """
         Handles the presence event from the API server.
@@ -74,7 +81,7 @@ class Client(object):
         if app_id != self._current_app_id:
             self._initialize_rpc(app_id)
         self._rpc.update(**presence)
-    
+
     def _on_new_token(self, new_token: str):
         """
         Handles the new token event from the API server.
@@ -84,16 +91,30 @@ class Client(object):
 
         with open(self._config_path, "w") as f:
             json.dump(self._config, f)
-        
+
         self._logger.info("New token retrieved from server!")
         self._logger.info(f"Your new token is {new_token}")
         self._logger.info("Provide this token to all apps you wish to use with WebRichPresence.")
-    
+
     def _on_authenticated(self, _):
         """
         Handles the authenticated event from the API server.
         """
-        self._logger.info("Connected and authenticated.")
+        self._logger.info("Authenticated.")
+
+    def _on_connected(self):
+        """
+        Handles the connect event from the API server.
+        """
+        self._logger.info("Connected to server. Authenticating...")
+        self._authenticate()
+
+    def _on_reconnect(self):
+        """
+        Handles the reconnect event from the API server.
+        """
+        self._logger.info("Regained connection to the API server. Re-authenticating...")
+        self._authenticate()
 
     def _authenticate(self):
         """
@@ -105,16 +126,13 @@ class Client(object):
         else:
             self._logger.info(f"Authenticating with existing token {self._config['token']}...")
             self._socket.emit("auth", self._config["token"])
-        
+
     def run(self):
         """
         Runs the client and connects to the API server.
         """
         self._logger.info("Connecting to WebRichPresence...")
         self._socket = SocketIO(self._hostname, self._port)
-        self._authenticate()
-        
-        for callback in self._callbacks.items():
-            self._socket.on(callback[0], callback[1])
+        self._socket.define(self._presence_namespace, "/presence")
 
         self._socket.wait()
